72c6f6905a59a324572625bd2f74cd7931207763
AIR <40839081+AndrewIRobles@users.noreply.github.com>
2018-10-27 11:31:59 -0500

    my commit

--- megs-morsels.js
+++ megs-morsels.js
@@ -10,3 +10,21 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+ });
+
+ app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "orders.html"));
+});
