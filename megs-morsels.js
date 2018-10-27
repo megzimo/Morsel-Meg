8d68e6682dd360f8f8f7168a1575872f10bf2ce8
Git-C <41130164+ckernan@users.noreply.github.com>
2018-10-27 11:21:37 -0500

    server working

--- megs-morsels.js
+++ megs-morsels.js
@@ -5,8 +5,27 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  app.get("/orders", function(req, res) {
+    res.sendFile(path.join(__dirname, "orders.html"));
+  });
+app.listen(PORT, function(){
+    console.log("App is listening on PORT: " + PORT)
+});
+
+//------Grab values from input form------
+
+// $(".submit").on("click" function(){
+
+// })
